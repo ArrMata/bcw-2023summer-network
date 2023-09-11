ff6e0e2ec15af12f56bd73ac5816a1727b53a54a
Arrington Mata <59297501+ArrMata@users.noreply.github.com>
2023-09-11 11:59:10 -0600

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://sandbox.codeworksacademy.com' : ''
+export const baseURL = 'https://sandbox.codeworksacademy.com'
 export const useSockets = false
 export const domain = 'codeworksclassroom.auth0.com'
 export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
